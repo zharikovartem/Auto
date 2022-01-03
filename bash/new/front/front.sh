9cf01fe525002f48b97bc4dcdb37715996ad744e
zharikovartem <7383125@mail.ru>
2022-01-03 15:03:48 +0300

    add back

--- bash/new/front/front.sh
+++ bash/new/front/front.sh
@@ -25,5 +25,12 @@ then
     touch start.sh
     echo .start.sh >> .gitignore
 
+    read -p "Нужно ли установить стартовые пакеты? y/n  > " startPocets
+    if [[ ${startPocets} == "y" ]]
+    then
+        npm i react-dom @types/react-dom react-router-dom @types/react-router-dom formik redux-thunk antd bootstrap react-bootstrap
+    fi
+
+    # Нужен ли докер для фронта
     bash ~/Desktop/Auto/bash/new/front/isDocker.sh
 fi
--- bash/new/run.sh
+++ bash/new/run.sh
@@ -1,11 +1,16 @@
+#!/bin/bash
 echo RUN INSTALL NEW PROJECT
 
 read -p "Нужен ли нам фронт? y/n   > " isfront
 
 if [[ ${isfront} == "y" ]]
 then
-    # touch package.json
-    # touch package-lock.json
+    bash ~/Desktop/Auto/bash/new/front/front.sh isfront
+fi
 
+read -p "Нужен ли нам бэк? y/n   > " isbackend
+
+if [[ ${isbackend} == "y" ]]
+then
     bash ~/Desktop/Auto/bash/new/front/front.sh isfront
 fi
