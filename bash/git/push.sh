85ee33197dbc866186c4b7178ae3487487460fc9
zharikovartem <7383125@mail.ru>
2022-01-03 14:23:35 +0300

    3

--- bash/git/push.sh
+++ bash/git/push.sh
@@ -9,5 +9,6 @@ read -p "Укажите имя коммита:   > " commit
 git add .
 git commit -m "${commit}"
 echo '7383125@mail.ru'
+echo 'zharikovartem'
 echo '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'
 git push
