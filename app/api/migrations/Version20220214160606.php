90845495d5ce0456f2013083698ad9b427572446
zharikovartem <7383125@mail.ru>
2022-02-14 19:07:17 +0300

    added db

---
+++ app/api/migrations/Version20220214160606.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20220214160606 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE `user` (id INT AUTO_INCREMENT NOT NULL, user_name VARCHAR(180) NOT NULL, roles JSON NOT NULL, UNIQUE INDEX UNIQ_8D93D64924A232CF (user_name), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE `user`');
+    }
+}
--- app/ui/src/App.tsx
+++ app/ui/src/App.tsx
@@ -1,18 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Menu from './Components/Menu'
+import Menu from './Components/Menu/Menu'
 import 'antd/dist/antd.css'
 
 function App() {
-  return (
-    <div className="App">
-      <Menu />
-      <li>Реализовать роутинг</li>
-      <li>Реализовать авторизацию</li>
-      <li>Реализовать реализовать обращение к бд</li>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Menu />
+            <li>Реализовать роутинг</li>
+            <li>Реализовать авторизацию</li>
+            <li>Реализовать реализовать обращение к бд</li>
+        </div>
+    )
 }
 
-export default App;
+export default App
