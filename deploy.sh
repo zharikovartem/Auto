66680f2e3e00fd273515d24c920cf338e31a3c8e
Artem Zharikov <a.zharikov@smartdesign.by>
2024-03-02 13:44:26 +0300

    backup_to_dev_at_02_03-(13:44)

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # clear; bash deploy.sh
+# 1
 
 read -p "Куде деплоим? (dev/prod):   > " envType
 
@@ -13,24 +14,21 @@ git pull
 git fetch
 # 2 Убираем все изменения
 git stash
-# 3 Переходим на целевую ветку
+# 3 Переходим на целевую ветку и заливаем комит
 git checkout $envType
 git merge $BACKUP_BRANCH
 git checkout $BACKUP_BRANCH
 git add .
 git commit -m "$COMMIT_NAME"
 git push
-
-# 4 Мержим целевую ветку в бэкап
-
+git checkout $envType
 # ?Возвращаем изменения
 git stash pop
+git add .
+git commit -m "$COMMIT_NAME"
+git push
 
 
 
 
 # #######################################
-# Имя ветки бэкапа
-echo "backup_$envType"
-# Имя коммита
-echo backup_to_${envType}_at_${CURRENTDATE}
